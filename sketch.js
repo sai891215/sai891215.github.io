a26c237d537f1836978a4609de056705a1c389a6
WU Shengzhi <328095481@qq.com>
2018-03-07 02:03:40 -0500

    final_final

--- sketch.js
+++ sketch.js
@@ -1,70 +1,35 @@
-// Daniel Shiffman
-// http://codingtra.in
-// Earthquake Data Viz
-// Video: https://youtu.be/ZiYdOwOrGyc
 var send = [];
 var mapimg;
-var img;
-var clat = 40.411153;
-var clon = -79.944564;
-
+var clat = 40.411053;
+var clon = -79.941564;
+var openImageLink;
+var openImage;
 var myDiv0, myDiv1;
 var ww = 1024;
-var hh = 1024;
+var hh = 1000;
 var l1, l2;
-var zoom = 13.9;
-var earthquakes;
+var zoom = 14.5;
 var cx, cy;
-var input;
-var data;
 var table;
 var imgArr = [];
 var httpsString = [];
-var img1;
-var myLon = [];
 var total = 2;
-var myLa = [];
 var jsonData = [];
 var GlobalData = [];
-var number = 0;
 var loadNumber = 0;
 var tabble2;
 var jsonData;
+var breath = 0;
+var logo, twitter;
 
 function preload() {
-  // img = loadImage("img1.jpeg");
-  table = loadTable('https://docs.google.com/spreadsheets/d/1lm1NtSv3NMnstnrst7WHbuRE-fSXPnDSDAMNKkp0pZM/edit?usp=sharing', 'csv', 'header');
 
-  // The clon and clat in this url are edited to be in the correct order.
-  mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/' +
-    clon + ',' + clat + ',' + zoom + '/' +
-    ww + 'x' + hh +
-    '?access_token=pk.eyJ1IjoiY29kaW5ndHJhaW4iLCJhIjoiY2l6MGl4bXhsMDRpNzJxcDh0a2NhNDExbCJ9.awIfnl6ngyHoB3Xztkzarw',GotImage);
-
-  Tabletop.init({
-    key: '1lm1NtSv3NMnstnrst7WHbuRE-fSXPnDSDAMNKkp0pZM',
-    callback: gotData,
-    simpleSheet: true
-  });
-
-
-  for (var i = 0; i < httpsString.length; i++) {
-    if (httpsString[i]) {
-
-      print("loading");
-      var li = httpsString[i];
-      // var img3= createImage("image.jpeg");  
-      var link = "https://api.rethumb.com/v1/exif/all/" + li;
-      print(link);
-      GlobalData[i] = loadJSON(link);
-    }
-  }
 
 }
 
 function gotData(stuff, tabletop) {
   data = stuff;
-  print("got data");
+
 }
 
 function mercX(lon) {
@@ -84,22 +49,40 @@ function mercY(lat) {
 
 
 function setup() {
+    mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/' +
+    clon + ',' + clat + ',' + zoom + '/' +
+    ww + 'x' + hh +
+    '?access_token=pk.eyJ1IjoiY29kaW5ndHJhaW4iLCJhIjoiY2l6MGl4bXhsMDRpNzJxcDh0a2NhNDExbCJ9.awIfnl6ngyHoB3Xztkzarw', GotImage);
+
+  Tabletop.init({
+    key: '1lm1NtSv3NMnstnrst7WHbuRE-fSXPnDSDAMNKkp0pZM',
+    callback: gotData,
+    simpleSheet: true
+  });
+
+
+
+
+  for (var i = 0; i < httpsString.length; i++) {
+    if (httpsString[i]) {
+
+
+      var li = httpsString[i];
+      var link = "https://api.rethumb.com/v1/exif/all/" + li;
+      GlobalData[i] = loadJSON(link);
+    }
+  }
+
   
-  var cnv=createCanvas(windowWidth, windowHeight);
+  
+  logo = loadImage("logo.png");
+  twitter = loadImage("twitter.png");
+
+  var cnv = createCanvas(windowWidth, windowHeight);
   cnv.style('display', 'block');
   translate(width / 2, height / 2);
   imageMode(CENTER);
-  // var ii = 1;
-  // while (table.getString(ii, 0).length > 50) {
-  //   // print(table.getString(ii, 0));
-  //   // var newHttps = join(splitTokens(trim(table.getString(ii, 0)), ' '), '');
-  //   httpsString[ii - 1] = newHttps;
-  //   URL = "https://api.rethumb.com/v1/exif/all/http://" + newHttps;
 
-  //   // print(newHttps);
-  //   ii++;
-  // }
- 
   cx = mercX(clon);
   cy = mercY(clat);
 
@@ -108,7 +91,7 @@ function setup() {
 
 
 this.gotdata = function(data) {
-  print("jasonLoading");
+  // print("jasonLoading");
   GlobalData[loadNumber] = data.GPS;
   print(GlobalData[loadNumber]);
 
@@ -117,7 +100,8 @@ this.gotdata = function(data) {
 this.toDecimal = function(number) {
   var n1 = split(number[0], '/')[0];
   var n2 = split(number[1], '/')[0];
-  var n3 = split(number[2], '/')[0] / 100;
+  var m3 = split(number[2], '/');
+  var n3 = m3[0] / m3[1];
 
   return (float(n1) + float(n2) / (60) + float(n3) / (3600));
 };
@@ -128,39 +112,28 @@ function gotData(stuff, tabletop) {
 
   for (var j = 0; j < jsonData.length; j++) {
     httpsString[j] = join(splitTokens(jsonData[j]["Web"], ' '), '');
-    print(httpsString[j]);
+    // print(httpsString[j]);
     imgArr[j] = createImg(httpsString[j]);
+    imgArr[j].class('image');
     imgArr[j].hide();
     send[j] = false;
-    print("img is added");
+    // print("img is added");
   }
-  // var header=createDiv("This is header");
-  // header.position(30,30);
-  // header.style('display','absolute');
 
 }
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function GotImage(mapping){
-  mapping=mapping;
-  
+function GotImage(mapping) {
+  mapping = mapping;
+
 }
 
 function draw() {
-   
-   
-  // if(windowWidth!=ww || windowHeight!=hh){
-    
-  //   ww=windowWidth;
-  //   hh=windowHeight;
-  //   print(ww);
-  //   loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/' +
-  //   clon + ',' + clat + ',' + zoom + '/' +
-  //   ww + 'x' + hh +
-  //   '?access_token=pk.eyJ1IjoiY29kaW5ndHJhaW4iLCJhIjoiY2l6MGl4bXhsMDRpNzJxcDh0a2NhNDExbCJ9.awIfnl6ngyHoB3Xztkzarw',GotImage);
-  //   }
+
+  breath += 0.02;
 
   if (jsonData != null) {
     if (GlobalData[loadNumber] && loadNumber < httpsString.length) {
@@ -168,21 +141,17 @@ function draw() {
       print("yes+1" + loadNumber);
     }
     if (!GlobalData[loadNumber] && send[loadNumber] == false) {
-
-
       var link = "https://api.rethumb.com/v1/exif/all/" + httpsString[loadNumber];
-
       print(link);
       loadJSON(link, gotdata);
-
       send[loadNumber] = true;
     }
   }
-  
-// var a=createA('http://p5js.org/', 'this is a link');
+
 
   translate(width / 2, height / 2);
   image(mapimg, 0, 0);
+
   imageMode(CENTER);
   //
   for (var j = 0; j < GlobalData.length; j++) {
@@ -195,45 +164,88 @@ function draw() {
       lat = la;
       var x = mercX(lon) - cx;
       var y = mercY(lat) - cy;
+      fill(254, 197, 20, 50);
+      // fill(255, 0, 100,50);
+      stroke(254, 197, 20);
+      // stroke(255,0,100);
+      ellipse(x, y, 25 + 10 * sin(breath), 25 + 10 * sin(breath));
+      // fill(255, 0, 100);
+      fill(254, 197, 30);
+      ellipse(x, y, 10, 10);
 
-      fill(255, 0, 100);
-      noStroke();
-      ellipse(x,y,10,10);
-      // var c=createA('');
-      // c.position(x +width/2,y+height/2);
-      // c.style('width','10px');
-      // c.style('height','10px');
-      // c.style('background-color','red');
       var distance = dist(mouseX, mouseY, x + width / 2, y + height / 2);
       if (distance < 20) {
-        
-       stroke(255);
+        stroke(255, 0, 100);
+        fill(255, 0, 100, 50);
+        ellipse(x, y, 25 + 10 * sin(breath), 25 + 10 * sin(breath));
+        // fill(255, 0, 100);
+        fill(255, 0, 100);
+        ellipse(x, y, 10, 10);
         // var img1=createImg('http://ift.tt/2F4UNAx',40,40);
-       
+
         var imgHeight = map(imgArr[j].height, 0, imgArr[j].height, 0, 120);
         var imgWidth = imgArr[0].width * imgHeight / imgArr[j].height;
 
-        image(imgArr[j], x, y - 60, imgWidth, imgHeight);
-        if(mouseIsPressed){
-          
-          window.open(httpsString[j].replace('download',''));
-          noLoop();
-        }
 
-      
-      } 
+        fill(254, 197, 30);
+        textSize(12);
+        textStyle(NORMAL);
+        stroke(254, 197, 30);
+        line(x, y + 80, x, 10 + y);
+        noStroke();
+
+        text(GlobalData[j].GPSDateStamp.replace(':', '.'), x, y + 100);
+
+        openImageLink = httpsString[j].replace('download', '');
+        image(imgArr[j], x, y - 80, imgWidth, imgHeight);
+      } else {
+        openImageLink = null;
+      }
     }
   }
-  textFont('Georgia');
-  textSize(36);
-  fill(255);
-  text("Data Visualization of Illegel Dumping in Hazelwood",0,-height/2+80);
+
+  fill(254, 197, 80);
+
+  // fill(255,);
+  noStroke();
+  textFont('Avenir');
+  textSize(25);
+  textAlign(LEFT);
+  textStyle(BOLD);
+  var textX = -ww / 2 + 30;
+  // textStyle("text-decoration","underline");
+  text("Data Visualization of Illegal Dumping in Hazelwood", textX, -height / 2 + 53);
+  textSize(12);
+  rect(textX, -height / 2 + 68, 350, 4);
+
+  fill(254, 197, 80, 190);
+  textStyle(NORMAL);
+  text("'Data Visualization of Illegal Dumping in Hazelwood' is a project made by students from Carnegie Mellon University. The project is aimed to address the illegal dumping problems in Hazelwood by encouraging residents to upload to our email address (cleanhazelwood@gmail.com), then utilize the data to visualize where do illegal dumping happen the most, and call people for action.",
+    textX, -height / 2 + 83, 350, 300);
+
+  textStyle(BOLD);
+  textSize(20);
+  fill(254, 197, 80);
   textAlign(CENTER);
-}
-function unHighLighted() {
- this.style('background-color','yellow');
-}
-function highLighted() {
- this.style("background-color","yellow")
+  text("Images We Got", 410, -height / 2 + 148);
+  textSize(100);
+  text(httpsString.length, 400, -height / 2 + 113);
+  // image(logo,-ww/2+160,hh/2-90,280,28);
+  // image(twitter,-ww/2+340,hh/2-90,35,30);
 }
 
+function mousePressed() {
+  window.open(openImageLink);
+}
+
+function unHighLighted() {
+  this.style('background-color', 'yellow');
+}
+
+function highLighted() {
+  this.style("background-color", "yellow")
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
